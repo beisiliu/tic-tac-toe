9bba74d7f80367be74b636e7a2345033be710f0c
beisiliu <93181099+beisiliu@users.noreply.github.com>
2023-08-14 09:55:45 +0800

    Update example02.cpp

--- Example/example02.cpp
+++ Example/example02.cpp
@@ -3,36 +3,6 @@
 
 const int SCREEN_WIDTH = 300;
 const int SCREEN_HEIGHT = 300;
-<<<<<<< HEAD
-
-SDL_Window* gWindow;
-
-bool init();
-
-bool init(const char *title, int x, int y, int w, int h, Uint32 flags)
-{
-    if ( SDL_Init(SDL_INIT_EVERYTHING) != 0 )
-    {
-        printf("SDL Init Error: %s \n", SDL_GetError());
-        return false;
-    }
-
-    gWindow = SDL_CreateWindow(title, x, y, w, h, flags);
-    if (gWindow == nullptr)
-    {
-        printf("SDL Create Window Error: %s \n", SDL_GetError());
-        return false;
-    }
-
-    return true;
-
-}
-
-int main(int argc, char* argv[])
-{
-    if ( init("example01", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_HEIGHT, SCREEN_WIDTH, SDL_WINDOW_SHOWN) )
-    {
-=======
 
 SDL_Window* gWindow;
 SDL_Surface* gSurface;
@@ -95,7 +65,6 @@ int main(int argc, char* argv[])
             SDL_UpdateWindowSurface( gWindow );
         }
 
->>>>>>> 705160eb9ac0a5e02be77b141154dc79ca895667
         SDL_Event e;
         bool isRunning = true;
         while(isRunning)
@@ -110,9 +79,5 @@ int main(int argc, char* argv[])
             }
         }
     }
-<<<<<<< HEAD
-=======
-
     close();
->>>>>>> 705160eb9ac0a5e02be77b141154dc79ca895667
 }
